43a59cd3eb6f262ffeb8454e424e098f0b3e7b45
macavilla <maca.villa.a@gmail.com>
2018-01-22 02:15:41 -0300

    playground: toggle visibility challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,91 @@
-"use strict";
+'use strict';
+
+console.log('App.js is running');
+
+var app = {
+    title: ' Indecision app',
+    subtitle: ' An app for indecision. Or not',
+    options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+var removeAll = function removeAll() {
+    app.options = [];
+    render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    Math.floor(Math.random() * app.options.length + 0);
+};
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && app.subtitle.length > 0 && React.createElement(
+            'h3',
+            null,
+            ' ',
+            app.subtitle,
+            '  '
+        ),
+        React.createElement(
+            'p',
+            null,
+            '  ',
+            app.options.length > 0 ? 'Here are your options:' : 'You have no options',
+            ' '
+        ),
+        React.createElement(
+            'button',
+            { onClick: onMakeDecision },
+            'What should I do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove all'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: app.options.indexOf(option) },
+                    ' ',
+                    option,
+                    ' '
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { action: '', onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+var appRoot = document.getElementById('app');
+render();
---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,42 @@
+const appRoot = document.getElementById('app');
+const app = {
+    title: 'Visibility toggle',
+    details: 'Here are some details...',
+    detailsAreShowed: ''
+};
+
+const toggleVisibility = () => {
+    app.detailsAreShowed = !app.detailsAreShowed;
+    render();
+    // My (very inefficient) solution:
+    //  if (!app.detailsAreShowed) {
+    //     app.detailsAreShowed = 'true';
+    //     render();
+    // } else {
+    //     app.detailsAreShowed = '';
+    //     render();
+    // }
+};
+
+const showDetails = () => {
+    if (app.detailsAreShowed) {
+        return app.details;
+    } else {
+        return ' ';
+    }
+render();
+};
+
+const render = () => {
+    const template = (
+        <div>
+            <h1> {app.title} </h1>
+            <button onClick={toggleVisibility}> {app.detailsAreShowed ? 'Hide' : 'Show'} details </button>
+            <p> {showDetails()} </p>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
